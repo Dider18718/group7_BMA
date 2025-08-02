1bd0fbdc186145a1649c79710600d7519afe3bab
shibli1123 <shibli1123@users.noreply.github.com>
2025-08-02 20:18:50 +0600

    git push

--- src/main/java/com/oop/groupseven/group7_bma/Shibli/MedicalInspectorDashboardViewController.java
+++ src/main/java/com/oop/groupseven/group7_bma/Shibli/MedicalInspectorDashboardViewController.java
@@ -1,7 +1,46 @@
 package com.oop.groupseven.group7_bma.Shibli;
 
+import javafx.event.ActionEvent;
+
 public class MedicalInspectorDashboardViewController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewHistory(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogInspection(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleGenerateReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleHazardEscalation(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleCleaningStatus(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleReportDamage(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleFuelCheck(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRoomStatus(ActionEvent actionEvent) {
+    }
+
+    public void handleReportDamage(ActionEvent actionEvent) {
+
+    }
+}

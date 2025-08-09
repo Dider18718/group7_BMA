2f255e88d44ac46e69800b9ad8609477d9f559e7
sujarna-m <sujarna04@gmail.com>
2025-08-09 21:22:14 +0600

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/oop/groupseven/group7_bma/Sujarna/CorporateHealthCoordinatorGoal4Controller.java
+++ src/main/java/com/oop/groupseven/group7_bma/Sujarna/CorporateHealthCoordinatorGoal4Controller.java
@@ -1,4 +1,29 @@
 package com.oop.groupseven.group7_bma.Sujarna;
 
+import javafx.event.ActionEvent;
+import javafx.scene.chart.BarChart;
+
 public class CorporateHealthCoordinatorGoal4Controller {
+    @javafx.fxml.FXML
+    private BarChart statsChart;
+
+    @javafx.fxml.FXML
+    public void downloadButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void timeComboBox(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void companyComboBox(ActionEvent actionEvent) {
+    }
 }
